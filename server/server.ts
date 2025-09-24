46ea796b1e0ef0ecdea7f54eec223ff656835e43
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-09-24 18:33:07 -0300

    feat: server file

---
+++ server/server.ts
@@ -0,0 +1,45 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+import cookieParser from 'cookie-parser'
+import connectDB from "./config/mongodb"
+import authRouter from "./routes/auth"
+import userRouter from "./routes/user"
+import errorRouter from "./routes/error"
+import homeRouter from "./routes/home"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+
+const port = process.env.PORT || 4000
+
+connectDB()
+
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', homeRouter)
+
+app.use('/auth', authRouter)
+
+app.use('/user', userRouter)
+
+app.use(errorRouter)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+})
