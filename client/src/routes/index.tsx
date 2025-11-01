bbf2b916f4786524fe38c53ecca99b564776ce00
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-11-01 17:37:24 -0300

    feat: adjust for new routes 'Fit' and 'Crypto'

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -6,6 +6,10 @@ import Signup from "../pages/Auth/Signup"
 import EmailVerify from "../pages/Auth/EmailVerify"
 import ResetPassword from "../pages/Auth/ResetPassword"
 import SystemsLayout from "../pages/Systems/SystemsLayout"
+import Fit from "../pages/Fit/Fit";
+import ExerciseDetail from "../pages/Fit/ExerciseDetail";
+import Crypto from "../pages/Crypto/Crypto";
+import CoinDetail from "../pages/Crypto/CoinDetail";
 
 const router = createBrowserRouter([
     {
@@ -21,8 +25,14 @@ const router = createBrowserRouter([
                 path: 'systems', element: <SystemsLayout />,
                 children: [
                     // { index: true, element: <Dashboard /> },
-                    // { path: 'fit', element: <Fit /> },
-                    // { path: 'crypto', element: <Crypto /> },
+                    { path: 'fit', children: [
+                        { index: true, element: <Fit /> },
+                        { path: 'exercise/:id', element: <ExerciseDetail /> },
+                    ] },
+                    { path: 'crypto', children: [
+                        { index: true, element: <Crypto /> },
+                        { path: 'coin/:id', element: <CoinDetail /> },
+                    ] },
                 ]
             }
         ]
