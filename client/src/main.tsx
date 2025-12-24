083d065095fc0ae578c2eb6bcfed4d880cf323b0
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-12-23 21:15:30 -0300

    feat: adding 'query provider'

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,13 @@ import { createRoot } from 'react-dom/client'
 import { AppContextProvider } from './context/AppContext.tsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/index.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './utils/systems/queryClient/http'
 
 createRoot(document.getElementById('root')!).render(
     <AppContextProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </AppContextProvider>
 )
