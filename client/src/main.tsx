1729ab5426d861e2e635a16f9d6219101a2a1706
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-03 21:49:16 -0300

    feat: implementing 'Suspense' and i18n for project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import './index.css'
+import './i18n'
 
+import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AppContextProvider } from './context/AppContext.tsx'
 import { RouterProvider } from 'react-router-dom'
@@ -8,9 +10,11 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './utils/systems/queryClient/http'
 
 createRoot(document.getElementById('root')!).render(
-    <AppContextProvider>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
-    </AppContextProvider>
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100">...</div>}>
+        <AppContextProvider>
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
+        </AppContextProvider>
+    </Suspense>
 )
