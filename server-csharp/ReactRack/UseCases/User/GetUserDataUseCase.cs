e665a328695ab2c5d51ba94573191e5d2fd60be7
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-22 13:50:21 -0300

    feat: get user data uses case

---
+++ server-csharp/ReactRack/UseCases/User/GetUserDataUseCase.cs
@@ -0,0 +1,21 @@
+﻿using Communication.Responses.UserData;
+using Communication.Responses.UserData.Interfaces;
+using Exceptions.NotFound;
+using ReactRack.Infrastructure.Repositories.Interfaces;
+using ReactRack.UseCases.User.Interfaces;
+
+namespace ReactRack.UseCases.User
+{
+    public sealed class GetUserDataUseCase(IUserRepository userRepository) : IGetUserDataUseCase
+    {
+        public async Task<IUserDataResponse> ExecuteAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            var user = await userRepository.FindByIdAsync(userId, cancellationToken)
+                ?? throw new NotFoundReactRackException("Usuário não encontrado.");
+
+            return new UserDataResponse(
+                true,
+                new UserDataPayload(user.Id, user.Name, user.Email, user.IsAccountVerified));
+        }
+    }
+}
