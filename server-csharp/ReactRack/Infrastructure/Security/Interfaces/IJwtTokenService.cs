2f6fc74cdfdd669fe077610d51595fe7517ed884
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-22 13:51:12 -0300

    feat: security in JWT .NET backend

---
+++ server-csharp/ReactRack/Infrastructure/Security/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿using ReactRack.Entities;
+
+namespace ReactRack.Infrastructure.Security.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        string Generate(User user);
+    }
+}
---
+++ server-csharp/ReactRack/Infrastructure/Security/JwtTokenService.cs
@@ -0,0 +1,39 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ReactRack.Entities;
+using ReactRack.Infrastructure.Security.Interfaces;
+using ReactRack.Infrastructure.Settings.Jwt;
+
+namespace ReactRack.Infrastructure.Security
+{
+    public sealed class JwtTokenService(IOptions<JwtOptions> options) : IJwtTokenService
+    {
+        private readonly JwtOptions _jwtOptions = options.Value;
+
+        public string Generate(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(7),
+                signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
