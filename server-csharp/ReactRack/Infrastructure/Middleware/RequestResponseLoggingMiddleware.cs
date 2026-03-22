681f04ce2cf22f1e8bc66efb689987da282c3fe8
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-22 13:52:28 -0300

    feat: logging middleware

---
+++ server-csharp/ReactRack/Infrastructure/Middleware/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace ReactRack.Infrastructure.Middleware
+{
+    public sealed class RequestResponseLoggingMiddleware(RequestDelegate next, ILogger<RequestResponseLoggingMiddleware> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            logger.LogInformation("Incoming request: {Method} {Path}", context.Request.Method, context.Request.Path);
+            await next(context);
+
+            stopwatch.Stop();
+            logger.LogInformation(
+                "Outgoing response: {StatusCode} in {ElapsedMilliseconds}ms",
+                context.Response.StatusCode,
+                stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
