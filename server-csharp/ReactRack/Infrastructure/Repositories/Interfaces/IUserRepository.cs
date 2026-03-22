cd640c872da58491f50a7e499cabf4f04a0d259f
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-22 13:54:00 -0300

    feat: user repository for application

---
+++ server-csharp/ReactRack/Infrastructure/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReactRack.Entities;
+
+namespace ReactRack.Infrastructure.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> FindByIdAsync(Guid userId, CancellationToken cancellationToken);
+        Task<User?> FindByEmailAsync(string email, CancellationToken cancellationToken);
+        Task CreateAsync(User user, CancellationToken cancellationToken);
+        Task UpdateAsync(User user, CancellationToken cancellationToken);
+    }
+}
---
+++ server-csharp/ReactRack/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactRack.Entities;
+using ReactRack.Infrastructure.Persistence;
+using ReactRack.Infrastructure.Repositories.Interfaces;
+
+namespace ReactRack.Infrastructure.Repositories
+{
+    public sealed class UserRepository(ReactRackDbContext reactRackDbContext) : IUserRepository
+    {
+        public async Task<User?> FindByIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            return await reactRackDbContext.Users.FirstOrDefaultAsync(user => user.Id == userId, cancellationToken);
+        }
+
+        public async Task<User?> FindByEmailAsync(string email, CancellationToken cancellationToken)
+        {
+            var sanitizedEmail = email.Trim().ToLowerInvariant();
+            return await reactRackDbContext.Users.FirstOrDefaultAsync(user => user.Email == sanitizedEmail, cancellationToken);
+        }
+
+        public async Task CreateAsync(User user, CancellationToken cancellationToken)
+        {
+            await reactRackDbContext.Users.AddAsync(user, cancellationToken);
+            await reactRackDbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(User user, CancellationToken cancellationToken)
+        {
+            reactRackDbContext.Users.Update(user);
+            await reactRackDbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
