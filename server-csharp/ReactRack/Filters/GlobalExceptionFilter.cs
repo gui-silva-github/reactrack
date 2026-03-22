5dd4a2d5fe4f58ad3411beb5b445ddfc4ab42fd9
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-22 13:39:20 -0300

    feat: exceptions for .NET backend

---
+++ server-csharp/ReactRack/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Communication.Responses.API;
+using Exceptions.ReactRack;
+
+namespace ReactRack.Filters
+{
+    public sealed class GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger) : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            if (exception is ReactRackException reactRackException)
+            {
+                context.Result = new ObjectResult(new APIResponse(false, reactRackException.Message))
+                {
+                    StatusCode = reactRackException.StatusCode
+                };
+                context.ExceptionHandled = true;
+                return;
+            }
+
+            logger.LogError(exception, "Unhandled exception");
+            context.Result = new ObjectResult(new APIResponse(false, "Erro interno no servidor."))
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+}
