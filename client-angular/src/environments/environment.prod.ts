a1170b23dbc407b749bbbc9fa93a35270a07f863
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-24 19:29:17 -0300

    feat: initial environment

---
+++ client-angular/src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  backendUrl: '',
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  },
+  cloudinary: {
+    cloudName: '',
+    uploadPreset: '',
+    apiKey: '',
+    apiSecret: ''
+  }
+};
+
---
+++ client-angular/src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: false,
+  backendUrl: '',
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  },
+  cloudinary: {
+    cloudName: '',
+    uploadPreset: '',
+    apiKey: '',
+    apiSecret: ''
+  }
+};
+
