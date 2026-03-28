667b099e28079bc3155a79ac823be306c7070b74
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-03-28 02:04:03 -0300

    feat: starting of graphql

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,16 @@
 import 'dotenv/config'
 import connectDB from "./config/mongodb"
+import { startGraphQL } from "./graphql/server"
 import app from "./app"
+import errorRouter from "./routes/error"
 
 const port = process.env.PORT || 4000
 
 connectDB()
 
-app.listen(port, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+startGraphQL(app).then(() => {
+    app.use(errorRouter)
+    app.listen(port, () => {
+        console.log(`Servidor rodando no endereço http://localhost:${port}`)
+    })
 })
