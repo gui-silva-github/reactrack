14c0d87a360c7fe8ed57567295d3ef5dd597e355
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-04-05 11:39:57 -0300

    feat: adding interceptor

--- client-angular/src/app/core/interceptors/http.interceptor.ts
+++ client-angular/src/app/core/interceptors/http.interceptor.ts
@@ -1,3 +1,19 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
-export const httpInterceptor: HttpInterceptorFn = (req, next) => next(req);
+export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+  const clonedReq = req.clone({
+    setHeaders: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return next(clonedReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status >= 500) {
+        console.log('Server Error: ', error);
+      }
+      return throwError(() => error);
+    })
+  )
+};
