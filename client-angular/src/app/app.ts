aa06a39fdde40db3771871e2127468504f342fc5
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2026-04-05 11:37:14 -0300

    refact: update html and ts files for app component

--- client-angular/src/app/app.ts
+++ client-angular/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar';
+import { ToastComponent } from './shared/components/toast/toast';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client-angular');
+  title = 'AngularRack';
+
+  private currentUrl = signal<string>('/');
+  isSystemsSection = computed(() => this.currentUrl().startsWith('/systems'));
+
+  constructor(private router: Router) {
+    this.currentUrl.set(this.router.url || '/');
+
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e) => this.currentUrl.set(e.urlAfterRedirects));
+    }
 }
