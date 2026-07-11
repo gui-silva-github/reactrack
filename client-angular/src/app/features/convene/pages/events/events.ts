e435dea8ddcdb07a5ba0c104b1819b3bac1d984a
Guilherme Silva <guilhermeviniciusbispodasilva@gmail.com>
2026-07-11 02:13:25 -0300

    feat: implement pre-final adjustments for v1.1 - part 2

--- client-angular/src/app/features/convene/pages/events/events.ts
+++ client-angular/src/app/features/convene/pages/events/events.ts
@@ -1,17 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { EventsIntro } from '../../components/events-intro/events-intro';
 import { FindEvent } from '../../components/find-event/find-event';
-import { Header } from '../../components/header/header';
 import { NewEvents } from '../../components/new-events/new-events';
-import { ConveneI18n } from '../../convene-i18n';
 
 @Component({
   selector: 'app-events',
-  imports: [RouterOutlet, Header, EventsIntro, NewEvents, FindEvent, RouterLink],
+  imports: [RouterOutlet, EventsIntro, NewEvents, FindEvent],
   templateUrl: './events.html',
   styleUrl: './events.css',
 })
-export class Events {
-  readonly t = inject(ConveneI18n);
-}
+export class Events {}
